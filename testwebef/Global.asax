d2c1ef6021becbb854082ddf1f6607e765056458
William <wyldebill00@hotmail.com>
2016-01-21 12:22:11 -0600

    initial commit

---
+++ testwebef/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testwebef.MvcApplication" Language="C#" %>
---
+++ testwebef/dal/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext()
+            : base("SchoolContext")
+        {
+            Database.SetInitializer<SchoolContext>(new SchoolInitializer());
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
